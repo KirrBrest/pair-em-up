d12ff0da139aaa352ddf01671d45e6b204a6f3e4
KirrBrest <lukashchuk.kiryl@yandex.by>
2025-11-14 02:00:17 +0300

    feat: Install Vite, EsLint, Prettier, Husky (Fri, Nov 14, 2025 02:00:17 AM)

---
+++ pair-em-up/eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly',
+        global: 'readonly',
+        window: 'readonly',
+        document: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+    },
+  },
+  {
+    ignores: ['node_modules/**', 'dist/**', 'build/**', '*.config.js'],
+  },
+];
